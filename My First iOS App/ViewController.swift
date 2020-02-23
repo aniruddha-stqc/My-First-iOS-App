17fe27022360ba22f73e45e49fb0819b08a76af6
biogenics <aniruddha2008@gmail.com>
2020-02-23 19:53:40 +0530

    Initial Commit

---
+++ My First iOS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by test on 23/02/20.
+//  Copyright © 2020 biogenics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
